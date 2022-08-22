e1d23ed8c043f504ffa8125a16fb91a68f1c14e8
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-08-21 23:36:35 -0300

    Primeiro commit

---
+++ primeiro-projeto-vendas/src/main/java/com/registrovendas/vendas/RegistroVendasApplication.java
@@ -0,0 +1,13 @@
+package com.registrovendas.vendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroVendasApplication.class, args);
+	}
+
+}
