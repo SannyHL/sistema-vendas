7149eba6de6217e434e8fa7314df15830d43e7ca
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-08-23 12:48:19 -0300

    Adição contrutor como ponto de injeção.

--- primeiro-projeto-vendas/src/main/java/com/registrovendas/vendas/services/RegistroVendasService.java
+++ primeiro-projeto-vendas/src/main/java/com/registrovendas/vendas/services/RegistroVendasService.java
@@ -1,13 +1,15 @@
 package com.registrovendas.vendas.services;
 
 import com.registrovendas.vendas.repository.RegistroVendasRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RegistroVendasService {
     // criação de um ponto de injeção do repository no service:
-    @Autowired
-    RegistroVendasRepository registroVendasRepository;
 
+    final RegistroVendasRepository registroVendasRepository;
+
+    public RegistroVendasService(RegistroVendasRepository registroVendasRepository) {
+        this.registroVendasRepository = registroVendasRepository;
+    }
 }
