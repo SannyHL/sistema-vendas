78df76ba705388aca5d7a78e3dc3f84452575933
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-08-24 22:20:32 -0300

    Inserção do beans @Repository.

--- primeiro-projeto-vendas/src/main/java/com/registrovendas/vendas/repository/RegistroVendasRepository.java
+++ primeiro-projeto-vendas/src/main/java/com/registrovendas/vendas/repository/RegistroVendasRepository.java
@@ -2,11 +2,14 @@ package com.registrovendas.vendas.repository;
 
 import com.registrovendas.vendas.model.RegistroVendasModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 //Essa Inteface está estendendo o JpaRepository, pois ele trás muitas facilidades de simplificação dos metodos, como delete.
 //O RegistroVendasRepository irá extender o JpaRepository<tipo de dominio que o repositorio gerencia, o tipo de identificador da entidade que o repositorio gerencia>
 //No caso como o repositorio irá gerenciar o Model, esse está sendo informardo, e o id dele também.
+@Repository
 public interface RegistroVendasRepository extends JpaRepository<RegistroVendasModel, UUID> {
+
 }
