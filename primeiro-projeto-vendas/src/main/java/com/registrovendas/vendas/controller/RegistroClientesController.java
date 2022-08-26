4ad85cd56a7435a3cc33d65d5df1a46a4dd655b7
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-08-25 22:55:09 -0300

    Criação metodo get e get{id}.

--- primeiro-projeto-vendas/src/main/java/com/registrovendas/vendas/controller/RegistroClientesController.java
+++ primeiro-projeto-vendas/src/main/java/com/registrovendas/vendas/controller/RegistroClientesController.java
@@ -2,6 +2,8 @@ package com.registrovendas.vendas.controller;
 
 import com.registrovendas.vendas.dto.RegistroClientesDto;
 import com.registrovendas.vendas.model.RegistroClientesModel;
+import com.registrovendas.vendas.repository.RegistroClientesRepository;
+import com.registrovendas.vendas.repository.RegistroEnderecoClientesRepository;
 import com.registrovendas.vendas.services.RegistroClientesService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @ResponseBody
@@ -17,9 +22,13 @@ import javax.validation.Valid;
 public class RegistroClientesController {
 
     @Autowired
+    private RegistroClientesRepository registroClientesRepository;
+
+    @Autowired
     private RegistroClientesService registroClientesService;
 
-    public RegistroClientesController(RegistroClientesService registroClientesService) {
+    public RegistroClientesController(RegistroEnderecoClientesRepository registroEnderecoClientesRepository, RegistroClientesRepository registroClientesRepository, RegistroClientesService registroClientesService) {
+        this.registroClientesRepository = registroClientesRepository;
         this.registroClientesService = registroClientesService;
     }
 
@@ -28,5 +37,20 @@ public class RegistroClientesController {
         var registroClientesModel = new RegistroClientesModel();
         BeanUtils.copyProperties(registroClientesDto, registroClientesModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(registroClientesService.created(registroClientesModel));
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<RegistroClientesModel>> getAll(){
+        return new ResponseEntity<List<RegistroClientesModel>>(registroClientesRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<RegistroClientesModel> getId(@PathVariable(value = "id")UUID id){
+        Optional<RegistroClientesModel> registroClientesModelOptional = registroClientesRepository.findById(id);
+        if (registroClientesModelOptional.isPresent()){
+            return new ResponseEntity<RegistroClientesModel>(registroClientesModelOptional.get(), HttpStatus.OK);
+        }else {
+            return new ResponseEntity<RegistroClientesModel>(HttpStatus.FOUND);
+        }
     }
 }
--- primeiro-projeto-vendas/src/main/java/com/registrovendas/vendas/services/RegistroClientesService.java
+++ primeiro-projeto-vendas/src/main/java/com/registrovendas/vendas/services/RegistroClientesService.java
@@ -2,7 +2,7 @@ package com.registrovendas.vendas.services;
 
 
 import com.registrovendas.vendas.model.RegistroClientesModel;
-import com.registrovendas.vendas.repository.ResgistroClientesRepository;
+import com.registrovendas.vendas.repository.RegistroClientesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,16 +11,16 @@ import javax.transaction.Transactional;
 @Service
 public class RegistroClientesService {
     @Autowired
-    private ResgistroClientesRepository resgistroClientesRepository;
+    private RegistroClientesRepository registroClientesRepository;
 
 
-    public RegistroClientesService(ResgistroClientesRepository resgistroClientesRepository) {
-        this.resgistroClientesRepository = resgistroClientesRepository;
+    public RegistroClientesService(RegistroClientesRepository registroClientesRepository) {
+        this.registroClientesRepository = registroClientesRepository;
     }
 
     @Transactional
     public RegistroClientesModel created(RegistroClientesModel registroClientesModel){
-        return resgistroClientesRepository.save(registroClientesModel);
+        return registroClientesRepository.save(registroClientesModel);
     }
 
 
