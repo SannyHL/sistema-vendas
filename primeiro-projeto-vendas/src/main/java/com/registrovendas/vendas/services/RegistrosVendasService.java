740797dfa6c58871e783a2f67ce0dab41965adb0
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-09-17 21:06:14 -0300

    Update RegistrosVendasService.java
    Reparando injeção de dependência.

--- primeiro-projeto-vendas/src/main/java/com/registrovendas/vendas/services/RegistrosVendasService.java
+++ primeiro-projeto-vendas/src/main/java/com/registrovendas/vendas/services/RegistrosVendasService.java
@@ -3,6 +3,7 @@ package com.registrovendas.vendas.services;
 import com.registrovendas.vendas.dto.RegistroVendasDto;
 import com.registrovendas.vendas.model.RegistroVendasModel;
 import com.registrovendas.vendas.repository.RegistroVendasRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -16,11 +17,8 @@ import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 public class RegistrosVendasService {
     // criação de um ponto de injeção do repository no service:
 
-    final RegistroVendasRepository registroVendasRepository;
-
-    public RegistrosVendasService(RegistroVendasRepository registroVendasRepository) {
-        this.registroVendasRepository = registroVendasRepository;
-    }
+    @Autowired
+    private RegistroVendasRepository registroVendasRepository;
 
     @Transactional
     public RegistroVendasModel create(RegistroVendasModel registroVendasModel) {
